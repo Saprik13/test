b0f633e43af42108b280f6c8cfd3b4a21e8231fb
Petro Sapronov <saprik13@gmail.com>
2022-01-26 01:39:25 +0200

    add google map

--- js/google-maps.js
+++ js/google-maps.js
@@ -1,8 +1,31 @@
-function myMap() {
-    var mapCanvas = document.getElementById("map");
-    var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.2),
-        zoom: 10
-    };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
+function initialize() {
+    var myLatlng = new google.maps.LatLng(50.4265807,30.5361971),
+    mapOptions = {
+        zoom: 17,
+        center: myLatlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+var map = new google.maps.Map(document.getElementById('map'), mapOptions),
+contentString = 'бул. Леси Украинки, 26, Киев, 02000',
+infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    maxWidth: 500
+});
+
+var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map
+});
+
+google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+});
+
+google.maps.event.addDomListener(window, "resize", function() {
+    var center = map.getCenter();
+    google.maps.event.trigger(map, "resize");
+    map.setCenter(center);
+    });
 }
+
+google.maps.event.addDomListener(window, 'load', initialize);
